f60d19e5733b5de27247c65f313da30c7922306c
Kesavan Rangarajan <k7rang2018@gmail.com>
2024-05-03 21:53:46 -0400

    conversation continuation is ehhh, also i think it might be crashing every once in a while

--- src/AI.py
+++ src/AI.py
@@ -1,5 +1,5 @@
 import speech_recognition as sr
-from openai import OpenAI
+# from openai import OpenAI
 from dotenv import load_dotenv
 import os
 import json
@@ -8,7 +8,7 @@ from messenger import *
 load_dotenv()
 
 
-client = OpenAI(base_url="https://localhost:4000/v1", api_key="not-needed")
+# client = OpenAI(base_url="https://localhost:4000/v1", api_key="not-needed")
 elevenlabsApiKey = os.getenv('ELEVENLABS_API_KEY')
 recognizer = sr.Recognizer()
 mic = sr.Microphone()
@@ -51,11 +51,17 @@ def listen(conversation_continuation_timeout=3):
             print("Recognizing...")
             statement = recognizer.recognize_google(audio)
             
-            if "spark" in statement:
+            if "spark" in statement.lower():
                 speak("Activated S.P.A.R.K.")
                 break
+            elif conversation_continuation_timeout > 3 and statement:
+                print("Still in Conversation")
+                break
+            elif conversation_continuation_timeout > 3 and not statement:
+                print("Conversation Timeout")
+                break
         except sr.UnknownValueError:
-            speak("Sorry, I couldn't understand. Please say that again.")
+            return ""
         except sr.RequestError:
             speak("Speech recognition service is unavailable. Please try again later.")
         except:
@@ -74,7 +80,7 @@ def send_to_chatGPT(messages, model="gpt-3.5-turbo"):
     return response.choices[0].message['content']
 messages = []
 messages.append(get_json_data()["messages"][0])
-speak(send_to_chatGPT(messages))
+# speak(send_to_chatGPT(messages))
 # exit_words = ["goodbye", "bye", "exit", "quit", "stop", "that's all", "that's it", "that is all", "that is it", "that's all for now", "that's it for now", "that is all for now", "that is it for now", "that's all, thanks", "that's it, thanks", "that is all, thanks", "that is it, thanks", "that's all for now, thanks", "that's it for now, thanks", "that is all for now, thanks", "that is it for now, thanks", "that's all, thank you", "that's it, thank you", "that is all, thank you", "that is it, thank you", "that's all for now, thank you", "that's it for now, thank you", "that is all for now, thank you", "that is it for now, thank you"]
 
 def main():
@@ -82,22 +88,23 @@ def main():
     while True:
         text = listen(conversation_continuation_timeout)
         if text:
-            messages.append({"role": "user", "content": text})
-            save_json_data({"messages": messages})
-            response = send_to_chatGPT(messages)
-            if "{" in response:
-                response = response.split("{")[1]
-                response = response.split("}")[0]
-                device, action, value = response.split(",")
-                device = device.split(":")[1].strip()
-                action = action.split(":")[1].strip()
-                value = value.split(":")[1].strip()
-                send_to_device(device, action, value)
+            # messages.append({"role": "user", "content": text})
+            # save_json_data({"messages": messages})
+            # response = send_to_chatGPT(messages)
+            # if "{" in response:
+            #     response = response.split("{")[1]
+            #     response = response.split("}")[0]
+            #     device, action, value = response.split(",")
+            #     device = device.split(":")[1].strip()
+            #     action = action.split(":")[1].strip()
+            #     value = value.split(":")[1].strip()
+            #     send_to_device(device, action, value)
                 
-            speak(response)
-            conversation_continuation_timeout = 10
+            # speak(response)
+            conversation_continuation_timeout = 5
         else:
             conversation_continuation_timeout = 3
 
 # if __name__ == "__main__":
 #     main()
+main()
