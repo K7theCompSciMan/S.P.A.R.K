80b572f4ea1400a53b4e68f3f9f6c1297b5bbe23
Kesavan Rangarajan <k7rang2018@gmail.com>
2024-07-23 17:30:32 -0400

    idk whats going on but I give up

--- client/src-tauri/src/main.rs
+++ client/src-tauri/src/main.rs
@@ -1,9 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{self, Manager};
+use std::{borrow::Borrow, sync::{Arc}, thread};
+use std::path::PathBuf; // Add this line to import PathBuf
+
+use backend::handle_device_updates;
+use tauri::{self, App, EventLoopMessage, Manager, Wry};
+use tauri_plugin_store::{with_store, StoreCollection};
 mod app;
 mod tray;
+mod backend;
+mod xata_structs;
 fn main() {
   tauri::Builder::default()
     .plugin(tauri_plugin_store::Builder::default().build())
@@ -16,7 +23,11 @@ fn main() {
         api.prevent_close();
       }
       _ => {}
-    })
+    }).setup(|app| Ok({
+      let _store = with_store(app.app_handle(), app.state::<StoreCollection>(),  PathBuf::from("C:/Code/S.P.A.R.K/client/stores/store.json"), |store| {
+        handle_device_updates<Wry<EventLoopMessage>>(store);
+      }).expect("failed to handle device updates");
+    }))
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 
