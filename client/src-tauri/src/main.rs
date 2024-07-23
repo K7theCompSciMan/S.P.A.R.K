173db51da027d17a7154173ee86b8f1009067b4a
Kesavan Rangarajan <k7rang2018@gmail.com>
2024-07-23 11:13:57 -0400

    system tray and hide on close

--- client/src-tauri/src/main.rs
+++ client/src-tauri/src/main.rs
@@ -1,10 +1,23 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::{self, Manager};
 mod app;
+mod tray;
 fn main() {
   tauri::Builder::default()
     .plugin(tauri_plugin_store::Builder::default().build())
     .invoke_handler(tauri::generate_handler![app::start_app, app::run_command])
+    .system_tray(tray::init_tray())
+    .on_system_tray_event(tray::system_tray_event_handler)
+    .on_window_event(|event| match event.event() {
+      tauri::WindowEvent::CloseRequested { api, .. } => {
+        event.window().hide().unwrap();
+        api.prevent_close();
+      }
+      _ => {}
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
+
 }
