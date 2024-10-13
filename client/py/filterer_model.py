d8b5fca729743d2e01638ca471e67cfdaaa30387
Kesavan Rangarajan <k7rang2018@gmail.com>
2024-10-12 23:31:48 -0400

    improved text-filtering neural network (current ~50% accuracy)

--- client/py/filterer_model.py
+++ client/py/filterer_model.py
@@ -8,20 +8,20 @@ from nltk.tokenize import word_tokenize
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 
-max_words = 10000
-max_length = 500
+max_words = 20000
+max_length = 50
 model = Sequential()
 model.add(Embedding(max_words, 32, input_length=max_length))
-model.add(Conv1D(32, 7, activation='relu'))
+model.add(Conv1D(128, 7, activation='relu'))
 model.add(MaxPooling1D(5))
-model.add(Conv1D(32, 7, activation='relu'))
+model.add(Conv1D(128, 7, activation='relu'))
 model.add(GlobalMaxPooling1D())
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.summary()
 
 
-df = pd.read_csv('/data/actions.csv')
+df = pd.read_csv('C:\\Code\\S.P.A.R.K\\client\\py\\actions.csv')
 df.head()
 df = df.drop_duplicates()
 df.groupby('IsAction').describe()
@@ -35,14 +35,13 @@ def remove_stop_words(text):
 x = df.apply(lambda row: remove_stop_words(row['Text']), axis=1)
 y = df['IsAction']
 
-max_words = 20000
-max_length = 500
+
 
 tokenizer = Tokenizer(num_words=max_words)
 tokenizer.fit_on_texts(x)
 sequences = tokenizer.texts_to_sequences(x)
 x = pad_sequences(sequences, maxlen=max_length)
-hist = model.fit(x, y, validation_split=0.2, epochs=5, batch_size=20)
+hist = model.fit(x, y, validation_split=0.2, epochs=10, batch_size=32)
 acc = hist.history['accuracy']
 val = hist.history['val_accuracy']
 epochs = range(1, len(acc) + 1)
@@ -53,5 +52,7 @@ plt.title('Training and Validation Accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.legend(loc='lower right')
-plt.plot()
+plt.show()
 
+
+print(model.predict([input("enter phrase: ")]))
