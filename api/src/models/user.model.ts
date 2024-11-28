d211f9a3cb29d180cd40b47c1ab823a0c1d245df
Kesavan Rangarajan <k7rang2018@gmail.com>
2024-11-28 11:56:16 -0500

    update public user model

--- api/src/models/user.model.ts
+++ api/src/models/user.model.ts
@@ -3,10 +3,18 @@ import { User } from "../xata";
 export class  PublicUser{
     id: string;
     username: string;
+    settings: UserSettings;
 
     constructor(user: User){
         this.id = user.id;
         this.username = user.username!;
+        this.settings = user.settings!;
+    }
+}
+export class UserSettings{
+    primaryCommunicationMethod: string;
+    constructor(settings: UserSettings){
+        this.primaryCommunicationMethod = settings.primaryCommunicationMethod;
     }
 }
 export default User
