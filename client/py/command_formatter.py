f4a84d3e9358e871186931e0c919a84db21f9dcc
Kesavan Rangarajan <k7rang2018@gmail.com>
2024-12-28 19:05:54 -0500

    command_formatter now takes into account device aliases

--- client/py/command_formatter.py
+++ client/py/command_formatter.py
@@ -7,37 +7,86 @@ class CommandFormatter:
     def __init__(self):
         self.devices = []
         self.command_aliases = {}
+        self.device_aliases = {}
 
-    def parse_device_list(self, input_text):
-        device_matches = re.findall(r'\[{.*?}\](?=\s*(?:\|\||$))', input_text)
-        print(device_matches)
-        devices = []
+    def parse_device_list(self, input_text, device_data=None):
+        if not device_data: 
+            device_matches = re.findall(r'\[{.*?}\](?=\s*(?:\|\||$))', input_text)
+            print(device_matches)
+            devices = []
         
-        for match in device_matches:
-            try:
-                json_str = match.replace("'", '"')
-                print(json_str)
-                device_data = json.loads(json_str)
-                for device in device_data:
+            for match in device_matches:
+                try:
+                    json_str = match.replace("'", '"')
+                    print(json_str)
+                    device_data = json.loads(json_str)
+                    for device in device_data:
+                        processed_commands = []
+                        aliases = device.get('aliases', [])
+                        for alias in aliases:
+                            self.device_aliases[alias.lower()] = device['name']
+                        self.device_aliases[device['name'].lower()] = device['name']
+                        for command in device['commands']:
+                            commandName = command['name']
+                            aliases = command.get('aliases', [])
+                            processed_commands.append(commandName)
+                            
+                            for alias in aliases:
+                                self.command_aliases[alias.lower()] = commandName
+                            self.command_aliases[commandName.lower()] = commandName
+                        
+                        devices.append({
+                            'name': device['name'],
+                            'commands': processed_commands
+                        })
+                except json.JSONDecodeError as e:
+                    print(f"JSON Error: {str(e)}")
+                    continue
+            
+            return devices
+        else:
+            devices = []
+            if device_data['clients']:
+                for client in device_data['clients']:
                     processed_commands = []
-                    for command in device['commands']:
-                        name = command['name']
+                    aliases = client.get('aliases', [])
+                    for alias in aliases:
+                        self.device_aliases[alias.lower()] = client['name']
+                    self.device_aliases[client['name'].lower()] = client['name']
+                    for command in client['commands']:
+                        commandName = command['name']
                         aliases = command.get('aliases', [])
-                        processed_commands.append(name)
+                        processed_commands.append(commandName)
                         
                         for alias in aliases:
-                            self.command_aliases[alias.lower()] = name
-                        self.command_aliases[name.lower()] = name
+                            self.command_aliases[alias.lower()] = commandName
+                        self.command_aliases[commandName.lower()] = commandName
                     
                     devices.append({
-                        'name': device['name'],
+                        'name': client['name'],
                         'commands': processed_commands
                     })
-            except json.JSONDecodeError as e:
-                print(f"JSON Error: {str(e)}")
-                continue
-        
-        return devices
+            if device_data['servers']:
+                for server in device_data['servers']:
+                    processed_commands = []
+                    aliases = server.get('aliases', [])
+                    for alias in aliases:
+                        self.device_aliases[alias.lower()] = server['name']
+                    self.device_aliases[server['name'].lower()] = server['name']
+                    for command in server['commands']:
+                        commandName = command['name']
+                        aliases = command.get('aliases', [])
+                        processed_commands.append(commandName)
+                        
+                        for alias in aliases:
+                            self.command_aliases[alias.lower()] = commandName
+                        self.command_aliases[commandName.lower()] = commandName
+                    
+                    devices.append({
+                        'name': server['name'],
+                        'commands': processed_commands
+                    })
+            return devices
     def parse_natural_language(self, text):
         nl_text = text.split("||")[0].strip()
 
@@ -57,8 +106,18 @@ class CommandFormatter:
 
     def find_matching_device(self, device_name, devices):
         device_names = [d["name"] for d in devices]
-        matches = get_close_matches(device_name, device_names, n=1, cutoff=0.6)
-        return matches[0] if matches else None
+        matches = get_close_matches(
+            device_name.lower(),
+            [name.lower() for name in self.device_aliases.keys()],
+            n=1,
+            cutoff=0.6
+        )
+        if not matches:
+            for name in device_names:
+                if f" {name.lower()} " in f" {device_name.lower()} ":
+                    print(f"Found close match: {name}")
+                    return name
+        return self.device_aliases[matches[0]] if matches else None
 
     def find_matching_command(self, command, device):
         matches = get_close_matches(
@@ -67,12 +126,14 @@ class CommandFormatter:
             n=1,
             cutoff=0.6
         )
-        
-        return self.command_aliases[matches[0]] if matches else None
+        if self.command_aliases[matches[0]] in device['commands']:
+            return self.command_aliases[matches[0]]
+        else:
+            return None
 
-    def parse_command(self, input_text):
+    def parse_command(self, input_text, device_data=None):
         try:
-            devices = self.parse_device_list(input_text)
+            devices = self.parse_device_list(input_text, device_data)
             if not devices:
                 return {"success": False, "error": "No device list found in input"}
 
@@ -93,7 +154,7 @@ class CommandFormatter:
             device = next(
                 (d for d in devices if d["name"] == matching_device_name), None
             )
-
+            print(device)
             matching_command = self.find_matching_command(command, device)
             if not matching_command:
                 return {
@@ -119,14 +180,14 @@ class CommandFormatter:
             }
 
         except Exception as e:
-            return {"success": False, "error": f"Error parsing command: {str(e)}"}
+            return {"success": False, "error": f"Error parsing command: {(e)}"}
 
 
 if __name__ == "__main__":
-    test_input = """launch editor on my PC || [{"name": "My PC", "commands": [{"name": "Launch Notepad", "aliases": ["editor", "notepad", "text editor"]},{"name": "Open Chrome", "aliases": ["browser", "chrome", "web"]}]}]"""
+    test_input = """launch editor on my laptop || [{"name": "My PC", aliases: ['my laptop'], "commands": [{"name": "Launch Notepad", "aliases": ["editor", "notepad", "text editor"]},{"name": "Open Chrome", "aliases": ["browser", "chrome", "web"]}]}] """
 
     formatter = CommandFormatter()
-    result = formatter.parse_command(test_input)
+    result = formatter.parse_command(test_input, device_data={"clients":[{"name": "My PC", 'aliases': ['laptop'] , "commands": [{"name": "Launch Notepad", "aliases": ["editor", "notepad", "text editor"]},{"name": "Open Chrome", "aliases": ["browser", "chrome", "web"]}]}], 'servers': []})
 
     if result["success"]:
         print("Input:", test_input)
