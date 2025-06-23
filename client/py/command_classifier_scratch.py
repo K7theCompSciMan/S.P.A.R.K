9bbd043b69d07091fef5cd2f2022fb641e9d9a1e
K7theCompSciMan <K7rang2018@gmail.com>
2025-06-23 10:15:18 -0400

    added oop to classifier

--- client/py/command_classifier_scratch.py
+++ client/py/command_classifier_scratch.py
@@ -4,66 +4,75 @@ import pandas as pd
 import numpy as np
 import string
 
-
-data  = None
-with open('client/py/nn_scratch_data.json', 'rb') as f:
-    data = pd.read_json(f)
-
-def postprocess(output):
-    indices = np.argmax(output, axis=1)
-    result = ""
-    if(indices[0] == 0):
-        result ='command'
-    else:
-        result ='not_command'
-    confidence = output[0][indices[0]]
-    return {
-        'result':result,
-        'confidence': float(confidence),
-    }
+class CommandClassifier:
+    def __init__(self, data_file):
+        self.data_file = data_file
+        self.data  = None
+        with open(self.data_file, 'rb') as f:
+            self.data = pd.read_json(f)
+        
+        self.max_len = 50
 
 
-max_len = 50
+        self.training_data = self.data[:400]
+        self.test_data = self.data[400:]
+        Network.Tokenizer.build_vocab(self.training_data['text'])
+
+        self.y_train = self.training_data['label'].apply(lambda x: [1, 0] if x == 'command' else [0, 1])
+        self.y_train = np.stack(self.y_train.to_numpy())
+        self.y_test = self.test_data['label'].apply(lambda x: [1, 0] if x == 'command' else [0, 1])
+        self.y_test = np.stack(self.y_test.to_numpy())
+
+        self.nn = Network.NeuralNetwork([
+            Network.Layer.Dense(self.max_len, 64, Network.ActivationFunction.ReLU),
+            Network.Layer.Dense(64, 2, Network.ActivationFunction.CombinedSoftmaxCrossEntropy)  
+        ], self.y_train, Network.Optimizer.Adam(learning_rate = .005, rate_decay=1e-7)) 
 
 
-training_data = data[:400]
-test_data = data[400:]
-Network.Tokenizer.build_vocab(training_data['text'])
-
-y_train = training_data['label'].apply(lambda x: [1, 0] if x == 'command' else [0, 1])
-y_train = np.stack(y_train.to_numpy())
-y_test = test_data['label'].apply(lambda x: [1, 0] if x == 'command' else [0, 1])
-y_test = np.stack(y_test.to_numpy())
-
-nn = Network.NeuralNetwork([
-    Network.Layer.Dense(max_len, 64, Network.ActivationFunction.ReLU),
-    Network.Layer.Dense(64, 2, Network.ActivationFunction.CombinedSoftmaxCrossEntropy)  
-], y_train, Network.Optimizer.Adam(learning_rate = .005, rate_decay=1e-7)) 
+        self.nn.set_preprocess(lambda x: Network.Tokenizer.pad_sequence(Network.Tokenizer.encode(x.translate(str.maketrans('','',string.punctuation)).lower()), self.max_len))
+        self.nn.set_postprocess(self.postprocess)
 
 
-nn.set_preprocess(lambda x: Network.Tokenizer.pad_sequence(Network.Tokenizer.encode(x.translate(str.maketrans('','',string.punctuation)).lower()), max_len))
-nn.set_postprocess(postprocess)
+        self.x_train = self.training_data['text'].apply(self.nn.preprocess)
+        self.x_train = np.stack(self.x_train.to_numpy())
 
+        self.x_test = self.test_data['text'].apply(self.nn.preprocess)
+        self.x_test = np.stack(self.x_test.to_numpy())
 
-x_train = training_data['text'].apply(nn.preprocess)
-x_train = np.stack(x_train.to_numpy())
-
-x_test = test_data['text'].apply(nn.preprocess)
-x_test = np.stack(x_test.to_numpy())
-
-
-
-
+    def load_model(self, file):
+        self.nn.load(file)
+    
+    def postprocess(self,output):
+        indices = np.argmax(output, axis=1)
+        result = ""
+        if(indices[0] == 0):
+            result ='command'
+        else:
+            result ='not_command'
+        confidence = output[0][indices[0]]
+        return {
+            'result':result,
+            'confidence': float(confidence),
+        }
+    
+    def predict(self, text):
+        padded = self.nn.preprocess(text)
+        prediction = self.nn.predict(padded)
+        return prediction
+    
+    def train(self, output_file):
+        self.nn.train(self.x_train, self.y_train, epochs=1000, output_file=output_file)
 
 
 
 if __name__ == "__main__":
-    nn.load('client/py/nn_scratch_model.json')
-    # history = nn.train(x_train, y_train, epochs=10000, output_file='client/py/nn_scratch_model.json')
-    # nn.validate(x_test, y_test)
+    classifier = CommandClassifier('client/py/nn_scratch_data.json')
+    classifier.load_model('client/py/nn_scratch_model.json')
+    # classifier.train('client/py/nn_scratch_data.json')
     while True:
-        text = input( "Enter text (or 'q' to quit): ")
+        text = input("Enter text (or 'q' to quit): ")
         if text.lower() == 'q':
             break
-        print(nn.predict(nn.preprocess(text).reshape(1, max_len)))
-
+        
+        result = classifier.predict(text)
+        print(result)
