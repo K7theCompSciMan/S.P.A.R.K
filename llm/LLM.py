c249098002f4006fb1c629a7654d4d73feb871e2
Kesavan Rangarajan <k7rang2018@gmail.com>
2026-02-21 11:44:51 -0500

    ocd

--- llm/LLM.py
+++ llm/LLM.py
@@ -192,11 +192,11 @@ inputs, targets = datapreprocessor.preprocess()
 
 test_inputs  = torch.tensor(
     [[0.43, 0.15, 0.89], # Your     (x^1)
-    [0.55, 0.87, 0.66], # journey  (x^2)
-    [0.57, 0.85, 0.64], # starts   (x^3)
-    [0.22, 0.58, 0.33], # with     (x^4)
-    [0.77, 0.25, 0.10], # one      (x^5)
-    [0.05, 0.80, 0.55]] # step     (x^6)
+    [0.55, 0.87, 0.66],  # journey  (x^2)
+    [0.57, 0.85, 0.64],  # starts   (x^3)
+    [0.22, 0.58, 0.33],  # with     (x^4)
+    [0.77, 0.25, 0.10],  # one      (x^5)
+    [0.05, 0.80, 0.55]]  # step     (x^6)
 )
 batch = torch.stack((test_inputs, test_inputs), dim=0)
 
